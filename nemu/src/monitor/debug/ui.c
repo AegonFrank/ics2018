63cccc1f9713872743a34d13fd9cceb7f83fcda0
tracer-ics2018 <tracer@njuics.org>
2018-09-11 06:47:42 +0000

    >  compile 151140015 fykdzh Linux 14dac74a56bc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  06:47:42 up  1:13,  3 users,  load average: 0.66, 0.46, 0.27 72302e43ca15874c92a57b0042560c008ce13235

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -38,6 +38,11 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args) {
+  cpu_exec(-1);
+  return 0;
+}
+
 static struct {
   char *name;
   char *description;
@@ -48,6 +53,7 @@ static struct {
   { "q", "Exit NEMU", cmd_q },
 
   /* TODO: Add more commands */
+  { "si", "Execute a given number of instructions, default 1", cmd_si },
 
 };
 
