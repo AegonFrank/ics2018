c3736ca230050f7f3f4243789f395a36ba6d9187
tracer-ics2018 <tracer@njuics.org>
2018-09-16 11:55:19 +0000

    >  compile 151140015 fykdzh Linux 14dac74a56bc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  11:55:19 up 28 min,  3 users,  load average: 0.21, 0.16, 0.24 b18bbd887be87845cf3e5296ac8241a4df1c4470

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -87,6 +87,7 @@ static bool make_token(char *e) {
 
         switch (rules[i].token_type) {
 	  case TK_NOTYPE: break;
+	  case TK_DEC: strncpy(tokens[nr_token].str, substr_start, substr_len);
           default: tokens[nr_token++].type = rules[i].token_type;
         }
 
