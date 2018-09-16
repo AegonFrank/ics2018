6d5df1ce0129a1111201e6cad6ff41626d788e4a
tracer-ics2018 <tracer@njuics.org>
2018-09-16 13:53:27 +0000

    >  compile 151140015 fykdzh Linux 14dac74a56bc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  13:53:27 up  2:26,  4 users,  load average: 0.50, 0.68, 0.44 4ac983de577fdc9c15f5e2005fb0b0fd184bfced

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -110,6 +110,10 @@ static bool make_token(char *e) {
   return true;
 }
 
+uint32_t eval(int begin, int end, bool *success) {
+  return 0;
+}
+
 uint32_t expr(char *e, bool *success) {
   if (!make_token(e)) {
     *success = false;
@@ -117,7 +121,6 @@ uint32_t expr(char *e, bool *success) {
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  *success = true;
 
-  return 0;
+  return eval(0, nr_token - 1, success);
 }
