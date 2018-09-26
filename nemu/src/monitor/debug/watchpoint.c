368ac87e76c2109303da49adf0180c9daa176412
tracer-ics2018 <tracer@njuics.org>
2018-09-26 13:45:49 +0000

    >  compile 151140015 fykdzh Linux 14dac74a56bc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  13:45:49 up  7:13,  4 users,  load average: 0.02, 0.03, 0.04 271ed761fe803a6a12c0cf336882749230a39dd

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -19,5 +19,30 @@ void init_wp_pool() {
 }
 
 /* TODO: Implement the functionality of watchpoint */
+WP* new_wp() {
+  if (free_ == NULL) {
+    assert(0);
+  }
+  WP* wp = free_;
+  free_ = free_->next;
+  wp->next = head;
+  head = wp;
+  return head; 
+}
+
+void free_wp(WP *wp) {
+  if (wp == head) {
+    head = head->next;
+  }
+  else {
+    WP* prev = head;
+    while (prev->next != wp) {
+      prev = prev->next;
+    }
+    prev->next = wp->next;
+  }
+  wp->next = free_;
+  free_ = wp;
+}
 
 
