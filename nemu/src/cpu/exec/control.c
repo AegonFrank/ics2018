13c096f04f92dd574a6e0bdd53d8c4fd524be13a
tracer-ics2018 <tracer@njuics.org>
2018-10-07 09:08:20 +0000

    >  compile 151140015 fykdzh Linux 14dac74a56bc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  09:08:20 up  4:51,  4 users,  load average: 0.04, 0.06, 0.09 82f624881bc0ccb3bb36c5ef46ff0ad6a55c16dd

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -26,6 +26,7 @@ make_EHelper(jmp_rm) {
 
 make_EHelper(call) {
   // the target address is calculated at the decode stage
+  printf("%d\n", decoding.jmp_eip);
   TODO();
 
   print_asm("call %x", decoding.jmp_eip);
