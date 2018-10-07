f33a685d95cd067dafd08ce28d9cbbd3f912ff55
tracer-ics2018 <tracer@njuics.org>
2018-10-07 13:40:22 +0000

    >  compile 151140015 fykdzh Linux 14dac74a56bc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  13:40:22 up  9:23,  4 users,  load average: 0.12, 0.06, 0.10 bf4c3812b3be9c763090dcd24ba655e999bc0cb8

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -197,7 +197,15 @@ make_rtl_setget_eflags(SF)
 
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   // eflags.ZF <- is_zero(result[width * 8 - 1 .. 0])
-  TODO();
+  if (width == 1) {
+    cpu.eflags.ZF = !(*result & 0xff);
+  }
+  else if (width == 2) {
+    cpu.eflags.ZF = !(*result & 0xffff);
+  }
+  else {
+    cpu.eflags.ZF = !*result;
+  }
 }
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
