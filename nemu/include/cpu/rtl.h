cc4fcf2de25a4d57af90cc57090a1e5e3746a4a1
tracer-ics2018 <tracer@njuics.org>
2018-10-07 13:54:14 +0000

    >  compile 151140015 fykdzh Linux 14dac74a56bc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  13:54:14 up  9:37,  4 users,  load average: 0.19, 0.12, 0.09 3ed0debcc68c9e8e8532027928183f32ae6a0f3

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -179,7 +179,7 @@ static inline void rtl_setrelopi(uint32_t relop, rtlreg_t *dest,
 
 static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- src1[width * 8 - 1]
-  TODO();
+  *dest = (*src1 >> (width * 8 - 1)) & 1;
 }
 
 #define make_rtl_setget_eflags(f) \
