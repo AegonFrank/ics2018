12e2e751c7cd4a37647e872699f74411997f623b
tracer-ics2018 <tracer@njuics.org>
2018-10-17 14:43:27 +0000

    >  compile 151140015 fykdzh Linux 14dac74a56bc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  14:43:27 up  2:01,  3 users,  load average: 0.02, 0.09, 0.08 56c4dcbd379c9b8b9c2fcc226e4f1b6bda9a272b

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -52,8 +52,11 @@ make_EHelper(sar) {
 }
 
 make_EHelper(shl) {
-  TODO();
-  // unnecessary to update CF and OF in NEMU
+  rtl_shl(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
+
+  rtl_update_ZFSF(&t2, id_dest->width);
+ // unnecessary to update CF and OF in NEMU
 
   print_asm_template2(shl);
 }
