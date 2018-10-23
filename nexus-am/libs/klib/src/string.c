370d8ec1e3a48ed8623e384b4ea6f5a2d0023139
tracer-ics2018 <tracer@njuics.org>
2018-10-23 14:24:25 +0000

    >  run 151140015 fykdzh Linux 14dac74a56bc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  14:24:25 up  1:39,  4 users,  load average: 0.10, 0.16, 0.12 a7d72945dadfde9721efc007e96872814cad4535

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -19,6 +19,7 @@ char* strcat(char* dst, const char* src) {
 }
 
 int strcmp(const char* s1, const char* s2) {
+  assert(s1 != NULL && s2 != NULL);
   while (*s1 == *s2) {
     if (!*s1) {
       return 0;
