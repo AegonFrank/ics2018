fe77dfad3b069f916b1d1af008209adc8e5cfa77
tracer-ics2018 <tracer@njuics.org>
2018-10-24 13:10:36 +0000

    >  run 151140015 fykdzh Linux 14dac74a56bc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  13:10:36 up  6:33,  3 users,  load average: 0.34, 0.23, 0.26 5efefc6f66a9ecb260592a497c9439873c7c313c

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -12,7 +12,11 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+  va_list ap;
+  va_start(ap, fmt);
+  int res = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return res;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
