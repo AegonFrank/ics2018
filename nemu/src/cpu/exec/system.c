786ebcd95d4eb6a6cc0aa77695b9d11fd40c87dc
tracer-ics2018 <tracer@njuics.org>
2018-10-27 13:28:54 +0000

    >  compile 151140015 fykdzh Linux 1154a942f1c9 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  13:28:54 up  1:44,  3 users,  load average: 0.06, 0.15, 0.17 ee4d6987596d4b6bd341526d163af329cc048d42

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -7,6 +7,10 @@ uint32_t pio_read_l(ioaddr_t addr);
 uint32_t pio_read_w(ioaddr_t addr);
 uint32_t pio_read_b(ioaddr_t addr);
 
+void pio_write_l(ioaddr_t addr, uint32_t data);
+void pio_write_w(ioaddr_t addr, uint32_t data);
+void pio_write_b(ioaddr_t addr, uint32_t data);
+
 make_EHelper(lidt) {
   TODO();
 
@@ -65,7 +69,15 @@ make_EHelper(in) {
 }
 
 make_EHelper(out) {
-  TODO();
+  if (id_src->width == 1) {
+    pio_write_b(id_dest->val, id_src->val);
+  }
+  else if (id_src->width == 2) {
+    pio_write_w(id_dest->val, id_src->val);
+  }
+  else {
+    pio_write_l(id_dest->val, id_src->val);
+  }
 
   print_asm_template2(out);
 
