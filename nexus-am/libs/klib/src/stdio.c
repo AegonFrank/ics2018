0e5dc6a6ba1627e83539610b630937e0454b9446
151140015-付毅科 <2397499637@qq.com>
2018-10-29 13:24:55 +0000

    implemented printf

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,7 +3,11 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-static char * itoa(int num, char *str, int radix) {
+#define PRINT_BUF_SIZE 1000
+
+static char print_buf[PRINT_BUF_SIZE];
+
+static char * itoa(int num, char *str, int radix, int width, char fill) {
   assert(num >= 0);
   static char ascii[] = "0123456789abcdef";
   int cnt = 0;
@@ -11,6 +15,9 @@ static char * itoa(int num, char *str, int radix) {
     str[cnt++] = ascii[num % radix];
     num /= radix;
   } while (num != 0);
+  while (cnt < width) {
+    str[cnt++] = fill;
+  }
   str[cnt] = '\0';
   for (int i = 0; i < cnt / 2; ++i) {
     char tmp = str[i];
@@ -21,31 +28,55 @@ static char * itoa(int num, char *str, int radix) {
 }
 
 int printf(const char *fmt, ...) {
-  return 0;
+  va_list ap;
+  va_start(ap, fmt);
+  int res = vsprintf(print_buf, fmt, ap);
+  va_end(ap);
+  assert(res < PRINT_BUF_SIZE);
+  for (char *p = print_buf; *p != '\0'; ++p) {
+    _putc(*p);
+  }
+  return res;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
   char *old = out;
   while (*fmt != '\0') {
     if (*fmt == '%') {
-      switch (*++fmt) {
-        case 'd': {
-          int i = va_arg(ap, int);
-          itoa(i, out, 10);
-          while (*out != '\0') {
-            ++out;
+      _Bool exit = false;
+      char fill = ' ';
+      int width = 0;
+      while (true) {
+        switch (*++fmt) {
+          case 'd': {
+            int i = va_arg(ap, int);
+            itoa(i, out, 10, width, fill);
+            while (*out != '\0') {
+              ++out;
+            }
+            exit = true;
+            break;
           }
+          case 's': {
+            char *s = va_arg(ap, char *);
+            while (*s != '\0') {
+              *out++ = *s++;
+            }
+            exit = true;
+            break;
+          }
+          default: 
+            if (*(fmt + 1) == 'd') {
+              width = *fmt - '0';
+              assert(0 <= width && width <= 9);
+            }
+            else {
+              fill = *fmt;
+            }
+        }
+        if (exit) {
           break;
         }
-        case 's': {
-          char *s = va_arg(ap, char *);
-          while (*s != '\0') {
-            *out++ = *s++;
-          }
-          break;
-        }
-        default: 
-          assert(0);
       }
       ++fmt;
     }
