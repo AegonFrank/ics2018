9c4bfb1f78b05ae5a1ff120590e04d46fd0c63d5
tracer-ics2018 <tracer@njuics.org>
2018-10-29 15:40:35 +0000

    >  run 151140015 fykdzh Linux 1154a942f1c9 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  15:40:35 up  3:39,  1 user,  load average: 0.09, 0.36, 0.40 343ff6528cd11a8544e44ac414fb8f3413040031

--- nexus-am/am/arch/x86-nemu/src/devices/timer.c
+++ nexus-am/am/arch/x86-nemu/src/devices/timer.c
@@ -4,12 +4,14 @@
 
 #define RTC_PORT 0x48
 
+static uint32_t boot_time;
+
 size_t timer_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_TIMER_UPTIME: {
       _UptimeReg *uptime = (_UptimeReg *)buf;
       uptime->hi = 0;
-      uptime->lo = inl(RTC_PORT);
+      uptime->lo = inl(RTC_PORT) - boot_time;
       return sizeof(_UptimeReg);
     }
     case _DEVREG_TIMER_DATE: {
@@ -27,4 +29,5 @@ size_t timer_read(uintptr_t reg, void *buf, size_t size) {
 }
 
 void timer_init() {
+  boot_time = inl(RTC_PORT);
 }
