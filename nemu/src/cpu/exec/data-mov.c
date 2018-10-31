ead380cfc5a222a0d8eb3060848098dfbd3cada2
tracer-ics2018 <tracer@njuics.org>
2018-10-31 11:32:27 +0000

    >  compile 151140015 fykdzh Linux 1154a942f1c9 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  11:32:27 up  4:24,  3 users,  load average: 0.22, 0.33, 0.40 a0f80bfeae0c8967808607bc467fbaee50f735

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -56,10 +56,14 @@ make_EHelper(cltd) {
 
 make_EHelper(cwtl) {
   if (decoding.is_operand_size_16) {
-    TODO();
+    rtl_lr(&t0, R_AL, 1);
+    rtl_sext(&t1, &t0, 2);
+    rtl_sr(R_AX, &t1, 2);
   }
   else {
-    TODO();
+    rtl_lr(&t0, R_AX, 2);
+    rtl_sext(&t1, &t0, 4);
+    rtl_sr(R_EAX, &t1, 4);
   }
 
   print_asm(decoding.is_operand_size_16 ? "cbtw" : "cwtl");
