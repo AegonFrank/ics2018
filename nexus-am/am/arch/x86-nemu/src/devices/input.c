59ba722e9147ce969a0d19dc7ad80726f5aaa035
tracer-ics2018 <tracer@njuics.org>
2018-10-31 14:50:58 +0000

    >  run 151140015 fykdzh Linux 1154a942f1c9 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  14:50:58 up  7:43,  3 users,  load average: 0.60, 0.76, 0.62 f6a342d36e5de3c87d49acaef5fc986064088354

--- nexus-am/am/arch/x86-nemu/src/devices/input.c
+++ nexus-am/am/arch/x86-nemu/src/devices/input.c
@@ -2,15 +2,12 @@
 #include <x86.h>
 #include <amdev.h>
 
-#define KBD_DATA_PORT 0x60
-#define KBD_STATUS_PORT 0x64
-
 size_t input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _KbdReg *kbd = (_KbdReg *)buf;
-      kbd->keydown = inl(KBD_STATUS_PORT);
-      kbd->keycode = kbd->keydown ? inl(KBD_DATA_PORT) : _KEY_NONE;
+      kbd->keydown = 1;
+      kbd->keycode = _KEY_NONE;
       return sizeof(_KbdReg);
     }
   }
