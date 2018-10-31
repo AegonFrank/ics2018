89a751f671103e7dc0395ddcf578d661a2045cf4
tracer-ics2018 <tracer@njuics.org>
2018-10-31 12:22:40 +0000

    >  compile 151140015 fykdzh Linux 1154a942f1c9 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  12:22:40 up  5:14,  3 users,  load average: 0.34, 0.22, 0.24 b254948c1694fa8a4787e49bf7bd9e25a9de33d8

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -88,7 +88,24 @@ make_EHelper(dec) {
 }
 
 make_EHelper(neg) {
-  TODO();
+  if (id_dest->val == 0) {
+    rtl_set_CF(&tzero);
+  }
+  else {
+    t1 = 1;
+    rtl_set_CF(&t1);
+  }
+  
+  t2 = -id_dest->val;
+  operand_write(id_dest, &t2);
+
+  rtl_update_ZFSF(&t2, id_dest->width);
+
+  rtl_xor(&t0, &id_dest->val, &id_src->val);
+  rtl_xor(&t1, &id_dest->val, &t2);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
 
   print_asm_template1(neg);
 }
