a815196a7e1f7b706f027c4a911fbbe0fdd8e043
tracer-ics2018 <tracer@njuics.org>
2018-11-01 07:08:39 +0000

    >  compile 151140015 fykdzh Linux 1154a942f1c9 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  07:08:39 up  1:01,  3 users,  load average: 0.13, 0.19, 0.26 3d0c6b5d6b4f84e21491c9855ede2c17494e1c

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -9,13 +9,25 @@
 
 uint8_t pmem[PMEM_SIZE];
 
+int is_mmio(paddr_t addr);
+uint32_t mmio_read(paddr_t addr, int len, int map_NO);
+void mmio_write(paddr_t addr, int len, uint32_t data, int map_NO);
+
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
+  int id = is_mmio(addr);
+  if (id != -1) {
+    return mmio_read(addr, len, id);
+  }
   return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
 }
 
 void paddr_write(paddr_t addr, uint32_t data, int len) {
+  int id = is_mmio(addr);
+  if (id != -1) {
+    mmio_write(addr, len, data, id);
+  }
   memcpy(guest_to_host(addr), &data, len);
 }
 
