502dfcf5dc0b14a776e7949c26828d2d8083fbec
tracer-ics2018 <tracer@njuics.org>
2018-11-01 06:59:26 +0000

    >  run 151140015 fykdzh Linux 1154a942f1c9 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  06:59:26 up 52 min,  3 users,  load average: 0.16, 0.29, 0.34 9562f804526a4a7b14397569b559962cc70cd0a

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -3,9 +3,7 @@
 #include <amdev.h>
 #include <klib.h>
 
-#define SCREEN_PORT 0x100 // Note that this is not the standard
-#define SCREEN_H 300
-#define SCREEN_W 400
+#define SCREEN_PORT 0x100
 
 static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
 
@@ -13,8 +11,8 @@ size_t video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _VideoInfoReg *info = (_VideoInfoReg *)buf;
-      info->width = SCREEN_W;
-      info->height = SCREEN_H;
+      info->width = inl(SCREEN_PORT) >> 16;
+      info->height = inw(SCREEN_PORT);
       return sizeof(_VideoInfoReg);
     }
   }
@@ -25,9 +23,7 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
-      int i;
-      int size = screen_width() * screen_height();
-      for (i = 0; i < size; i ++) fb[i] = i;
+
       if (ctl->sync) {
         // do nothing, hardware syncs.
       }
