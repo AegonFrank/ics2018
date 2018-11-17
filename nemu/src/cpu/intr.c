001455b08b9d7b83ec9c07f54c5d4ce4f7de58e5
tracer-ics2018 <tracer@njuics.org>
2018-11-17 13:35:36 +0000

    >  compile 151140015 fykdzh Linux 1154a942f1c9 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  13:35:36 up 12:21,  3 users,  load average: 0.23, 0.21, 0.19 7054124d6fc4ee629575b8e11ccaa77192f0afce

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -8,6 +8,10 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   rtl_push((rtlreg_t *) &cpu.eflags);
   rtl_push((rtlreg_t *) &cpu.cs);
   rtl_push((rtlreg_t *) &ret_addr);
+  vaddr_t offset = 0;
+  offset += vaddr_read(cpu.idtr.base + NO * 8, 2);
+  offset += vaddr_read(cpu.idtr.base + NO * 8 + 6, 2) << 16;
+  rtl_j(offset);
 }
 
 void dev_raise_intr() {
