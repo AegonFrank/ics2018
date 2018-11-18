79aa35bedae3b0cd0af0a5d8c0aa128bc9a05964
tracer-ics2018 <tracer@njuics.org>
2018-11-18 14:11:07 +0000

    >  run 151140015 fykdzh Linux 1154a942f1c9 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  14:11:07 up  1:32,  3 users,  load average: 0.11, 0.10, 0.13 f7f1e833216be622b824ec8c3d710ec4acc1e9

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -9,10 +9,10 @@
 typedef uint32_t size_t;
 
 struct _Context {
+  struct _Protect *prot;
   uintptr_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
   int       irq;
   uintptr_t err, eip, cs, eflags;
-  struct _Protect *prot;
 };
 
 #define GPR1 eax
