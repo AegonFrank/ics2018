f3b6e178394b98952342fcdc115280f7b76f6a19
tracer-ics2018 <tracer@njuics.org>
2018-11-29 06:49:53 +0000

    >  run 151140015 fykdzh Linux 1154a942f1c9 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  06:49:53 up  4:38,  3 users,  load average: 0.27, 0.20, 0.15 4f24cc69e3adadc2285641986690e9dfcd8aa3a9

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -67,5 +67,16 @@ size_t fs_filesz(int fd) {
 }
 
 size_t fs_lseek(int fd, size_t offset, int whence) {
-  TODO();
+  switch(whence) {
+    case SEEK_SET:
+      file_table[fd].open_offset = offset;
+      break;
+    case SEEK_CUR:
+      file_table[fd].open_offset += offset;
+      break;
+    case SEEK_END:
+      file_table[fd].open_offset = file_table[fd].size + offset;
+      break;
+  }
+  return file_table[fd].open_offset;
 }
