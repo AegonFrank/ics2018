76d0ddb6642f72f361cd9da9c1072baa21eefecd
tracer-ics2018 <tracer@njuics.org>
2018-11-29 07:24:46 +0000

    >  run 151140015 fykdzh Linux 1154a942f1c9 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  07:24:46 up  5:13,  3 users,  load average: 0.28, 0.30, 0.22 dd3cf08667a88bb484a94b52ecf69b6ca085d50

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -50,7 +50,9 @@ int fs_open(const char *pathname, int flags, int mode) {
 }
 
 size_t fs_read(int fd, void *buf, size_t len) {
-  assert(file_table[fd].open_offset + len <= file_table[fd].size);
+  if (file_table[fd].open_offset + len > file_table[fd].size) {
+    len = file_table[fd].size - file_table[fd].open_offset;
+  }
   size_t start = file_table[fd].disk_offset + file_table[fd].open_offset;
   size_t res = ramdisk_read(buf, start, len);
   assert(res == len);
