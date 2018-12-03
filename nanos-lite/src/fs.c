1c0faa708bec27048ee48bae7bddc01532e649af
tracer-ics2018 <tracer@njuics.org>
2018-12-03 06:18:48 +0000

    >  run 151140015 fykdzh Linux 1154a942f1c9 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  06:18:48 up 15 min,  1 user,  load average: 0.40, 0.53, 0.60 77dcf5cff5a17bab3221de12cae0ce6b8518f770

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -36,6 +36,8 @@ static Finfo file_table[] __attribute__((used)) = {
   {"stdin", 0, 0, 0, invalid_read, invalid_write},
   {"stdout", 0, 0, 0, invalid_read, serial_write},
   {"stderr", 0, 0, 0, invalid_read, serial_write},
+  {"/dev/fb", 0, 0, 0, invalid_read, invalid_write},
+  {"/dev/events", 0, 0, 0, invalid_read, invalid_write},
   {"/proc/dispinfo", 128, 0, 0, dispinfo_read, invalid_write},
 #include "files.h"
 };
