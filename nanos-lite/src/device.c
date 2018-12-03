3e11b2764b858818fb9f6f14065841918cfe0e50
tracer-ics2018 <tracer@njuics.org>
2018-12-03 06:23:50 +0000

    >  run 151140015 fykdzh Linux 1154a942f1c9 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  06:23:50 up 20 min,  1 user,  load average: 0.30, 0.44, 0.55 114fe11b42f735dd12be0182bc6ebb87df6ace4

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -28,6 +28,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
+  TODO();
   return 0;
 }
 
--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -21,6 +21,8 @@ size_t serial_write(const void *buf, size_t offset, size_t len);
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len);
 
+size_t fb_write(const void *buf, size_t offset, size_t len);
+
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
   return 0;
@@ -36,7 +38,7 @@ static Finfo file_table[] __attribute__((used)) = {
   {"stdin", 0, 0, 0, invalid_read, invalid_write},
   {"stdout", 0, 0, 0, invalid_read, serial_write},
   {"stderr", 0, 0, 0, invalid_read, serial_write},
-  {"/dev/fb", 0, 0, 0, invalid_read, invalid_write},
+  {"/dev/fb", 0, 0, 0, invalid_read, fb_write},
   {"/dev/events", 0, 0, 0, invalid_read, invalid_write},
   {"/proc/dispinfo", 128, 0, 0, dispinfo_read, invalid_write},
 #include "files.h"
