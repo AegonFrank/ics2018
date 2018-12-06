72e5132303dd19d36f8a38691ac0995fd448945d
151140015-付毅科 <2397499637@qq.com>
2018-12-06 03:12:20 +0000

    debug

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -10,7 +10,7 @@ void vecnull();
 _Context* irq_handle(_Context *tf) {
   _Context *next = tf;
   if (user_handler) {
-    _Event ev;
+    _Event ev = {0};
     switch (tf->irq) {
       case 0x80: ev.event = _EVENT_SYSCALL; break;
       case 0x81: ev.event = _EVENT_YIELD; break;
