d182116f63c72022623960ec2efb91330a2c385c
tracer-ics2018 <tracer@njuics.org>
2018-12-14 08:42:52 +0000

    >  run 151140015 fykdzh Linux 1154a942f1c9 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  08:42:52 up  6:42,  1 user,  load average: 0.42, 0.86, 0.56 2b70237fc610bce8eea9d78ad07868727f896a1

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,11 +1,12 @@
 #include "common.h"
 
 _Context* do_syscall(_Context *c);
+_Context* schedule(_Context *prev);
 
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
     case _EVENT_SYSCALL: do_syscall(c); break;
-    case _EVENT_YIELD: Log("Handled event _EVENT_YIELD"); break;
+    case _EVENT_YIELD: Log("Handled event _EVENT_YIELD"); return schedule(c);
     default: panic("Unhandled event ID = %d", e.event);
   }
 
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -6,7 +6,7 @@ static PCB pcb[MAX_NR_PROC] __attribute__((used));
 static PCB pcb_boot;
 PCB *current;
 
-void naive_uload(PCB *pcb, const char *filename);
+void context_kload(PCB *pcb, void *entry);
 
 void switch_boot_pcb() {
   current = &pcb_boot;
@@ -22,9 +22,12 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-  naive_uload(NULL, "/bin/init");
+  context_kload(&pcb[0], (void *)hello_fun);
+  switch_boot_pcb();
 }
 
 _Context* schedule(_Context *prev) {
-  return NULL;
+  current->cp = prev;
+  current = &pcb[0];
+  return current->cp;
 }
