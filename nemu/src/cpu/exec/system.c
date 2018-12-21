6a7e832e2d0a7fe25402e196061260d490406ddf
tracer-ics2018 <tracer@njuics.org>
2018-12-21 09:34:58 +0000

    >  compile 151140015 fykdzh Linux 1154a942f1c9 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  09:34:58 up  6:55,  3 users,  load average: 0.09, 0.24, 0.40 6f9d315e4d1efa2ed5e0801ae8900d79c9b3fa2

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -26,7 +26,9 @@ make_EHelper(lidt) {
 }
 
 make_EHelper(mov_r2cr) {
-  panic("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
+  // TODO();
+
+  print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
 make_EHelper(mov_cr2r) {
