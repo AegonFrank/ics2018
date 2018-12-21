3a7aa0ccb8c2ceacfb5896630ecf38b8b609a4bc
tracer-ics2018 <tracer@njuics.org>
2018-12-21 09:38:09 +0000

    >  compile 151140015 fykdzh Linux 1154a942f1c9 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  09:38:09 up  6:58,  3 users,  load average: 0.37, 0.27, 0.38 a8738573a2f090976455e486ef2a0274481209c5

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -28,13 +28,13 @@ make_EHelper(lidt) {
 make_EHelper(mov_r2cr) {
   // TODO();
 
-  print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
+  panic("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
 make_EHelper(mov_cr2r) {
-  // TODO();
+  TODO();
 
-  panic("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
+  print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
 
 #if defined(DIFF_TEST)
   difftest_skip_ref();
