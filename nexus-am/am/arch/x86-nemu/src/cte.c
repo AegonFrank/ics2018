cb401f394647bcfec4956f7a6e11db429953ad83
tracer-ics2018 <tracer@njuics.org>
2018-12-27 14:25:35 +0000

    >  run 151140015 fykdzh Linux 1154a942f1c9 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  14:25:35 up  3:49,  1 user,  load average: 0.07, 0.12, 0.09 215d82e2cfd368ec300ff180b5bc35f975006d87

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -8,6 +8,7 @@ void vectrap();
 void vecnull();
 
 void get_cur_as(_Context *c);
+void _switch(_Context *c);
 
 _Context* irq_handle(_Context *tf) {
   get_cur_as(tf);
@@ -25,7 +26,7 @@ _Context* irq_handle(_Context *tf) {
       next = tf;
     }
   }
-
+  _switch(next);
   return next;
 }
 
