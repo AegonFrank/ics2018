20087c2129193ff92a734f523f4808c42b921c15
tracer-ics2018 <tracer@njuics.org>
2018-12-28 03:00:00 +0000

    >  run 151140015 fykdzh Linux 1154a942f1c9 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  03:00:00 up  2:41,  1 user,  load average: 0.05, 0.14, 0.24 7ea389d398c205d23b41953c72f8e09abef6c20b

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -1,4 +1,5 @@
 #include "memory.h"
+#include "proc.h"
 
 static void *pf = NULL;
 
@@ -15,6 +16,17 @@ void free_page(void *p) {
 
 /* The brk() system call handler. */
 int mm_brk(uintptr_t new_brk) {
+  Log("cur_brk is 0x%x, max_brk is 0x%x, new_brk is 0x%x", current->cur_brk, current->max_brk, new_brk);
+  if (new_brk > current->max_brk) {
+    uintptr_t page_begin = (current->max_brk & 0xfffff000) + 4096;
+    uintptr_t page_end = (new_brk & 0xfffff000) + 4096;
+    for (uintptr_t va = page_begin; va != page_end; va += 4096) {
+      void *pa = new_page(1);
+      _map(&current->as, (void *) va, pa, 0);
+    }
+    current->max_brk = new_brk;
+  }
+  current->cur_brk = new_brk;
   return 0;
 }
 
