d4d8c32ee12a5421a4f178af71a618359408d923
tracer-ics2018 <tracer@njuics.org>
2018-12-28 06:08:16 +0000

    >  run 151140015 fykdzh Linux 1154a942f1c9 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  06:08:16 up  2:01,  1 user,  load average: 0.15, 0.41, 0.39 c987ceab2ea4a4139d102322ad5ef93fdd4fd769

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,8 +6,8 @@ _Context* schedule(_Context *prev);
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
     case _EVENT_SYSCALL: do_syscall(c); break;
-    case _EVENT_YIELD: Log("Handled event _EVENT_YIELD"); return schedule(c);
-    case _EVENT_IRQ_TIMER: _yield(); break;
+    case _EVENT_YIELD: return schedule(c);
+    case _EVENT_IRQ_TIMER: Log("_EVENT_IRQ_TIMER"); _yield(); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
