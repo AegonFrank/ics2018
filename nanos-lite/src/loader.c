ead11e466c7b0724988d6e91fac5db66323d400c
tracer-ics2018 <tracer@njuics.org>
2018-12-29 02:04:48 +0000

    >  run 151140015 fykdzh Linux 1154a942f1c9 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  02:04:48 up 14 min,  1 user,  load average: 0.13, 0.38, 0.48 4c152287edd0e04937ae6989ee76ef5e758a6a04

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -6,6 +6,7 @@ int fs_open(const char *pathname, int flags, int mode);
 size_t fs_read(int fd, void *buf, size_t len);
 int fs_close(int fd);
 size_t fs_filesz(int fd);
+int mm_brk(uintptr_t new_brk);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
   int fd = fs_open(filename, 0, 0);
@@ -23,8 +24,12 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
-  uintptr_t entry = loader(pcb, filename);
-  ((void(*)())entry) ();
+  int fd = fs_open(filename, 0, 0);
+  size_t size = fs_filesz(fd);
+  mm_brk(DEFAULT_ENTRY + size);
+  fs_read(fd, (void *) DEFAULT_ENTRY, size);
+  fs_close(fd);
+  ((void(*)())DEFAULT_ENTRY) ();
 }
 
 void context_kload(PCB *pcb, void *entry) {
